eb15f1d8479a064203465366bd92add47a48085a
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-02-19 17:10:20 +0800

    Manual exit code management

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -9,15 +9,32 @@ use args::*;
 use preset::*;
 
 #[paw::main]
-fn main(args: Args) -> anyhow::Result<()> {
+fn main(args: Args) {
     env_logger::init();
 
+    std::process::exit(match inner_main(args) {
+        Ok(()) => 0,
+        Err(e) => {
+            log::error!("{:?}", e);
+            1
+        }
+    });
+}
+
+fn inner_main(args: Args) -> anyhow::Result<()> {
     let Args {
         cmd, storage,
     } = args;
 
     match cmd {
-        Command::Apply { preset, recipe, .. } => {
+        Command::Apply {
+            preset,
+            recipe,
+            params,
+            non_interactive,
+            no_overwrite_backup,
+            dry_run,
+        } => {
             let preset_path = preset.as_ref().map(|p| {
                 let mut path = storage.clone();
                 path.push("presets");
@@ -28,18 +45,22 @@ fn main(args: Args) -> anyhow::Result<()> {
             recipe_path.push("recipes");
             recipe_path.push(recipe.clone() + ".yaml");
 
-            let recipe_file: Recipe = serde_yaml::from_reader(File::open(&recipe_path)?)?;
+            let recipe_file: Recipe = serde_yaml::from_reader(File::open(&recipe_path).map_err(
+                |_| anyhow::anyhow!("Recipe not found: {}", recipe)
+            )?)?;
             let preset_file: Option<Preset> = match preset_path {
                 None => None,
-                Some(p) => serde_yaml::from_reader(File::open(&p)?)?,
+                Some(p) => serde_yaml::from_reader(File::open(&p).map_err(
+                    |_| anyhow::anyhow!("Preset not found: {}", preset.clone().unwrap())
+                )?)?,
             };
             log::debug!("{:#?}", recipe_file);
             log::debug!("{:#?}", preset_file);
 
-            let params = if let Some(mut pf) = preset_file {
+            let mut joint_params = if let Some(mut pf) = preset_file {
                 match pf.presets.remove(&recipe) {
                     None => {
-                        println!("Preset `{}` is not applicable to recipe `{}`. Use the subcommand `show preset` to show applicable recipes.", preset.unwrap(), recipe);
+                        log::error!("Preset `{}` is not applicable to recipe `{}`. Use the subcommand `show preset` to show applicable recipes.", preset.unwrap(), recipe);
                         return Err(anyhow::anyhow!("Invalid parameters"));
                     },
                     Some(inner) => inner,
@@ -47,6 +68,9 @@ fn main(args: Args) -> anyhow::Result<()> {
             } else {
                 HashMap::new()
             };
+
+            joint_params.extend(params.into_iter());
+
             unimplemented!()
         },
         Command::List(_) => unimplemented!(),
