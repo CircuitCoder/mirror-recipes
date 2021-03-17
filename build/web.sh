e0532f6543a95fb51fa7e3d68d046a3c17f2493c
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-03-17 18:00:04 +0800

    Manually linking preact

--- build/web.sh
+++ build/web.sh
@@ -3,11 +3,13 @@ set -e
 mkdir -p output
 
 cd web/renderer
+mkdir node_modules
 yarn link
 
 cd ../homepage
 yarn --frozen-lockfile
 yarn link mirror-recipes-renderer
+ln -s $(readlink -f node_modules/preact) ../renderer/node_modules/preact
 yarn build
 
 cp -r build/* ../../output
