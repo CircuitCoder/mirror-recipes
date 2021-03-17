bb8aa32444b3ee1744128c06c1e50e6fa0ad0ce8
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-03-17 17:45:39 +0800

    Fixing action

--- build/web.sh
+++ build/web.sh
@@ -2,11 +2,12 @@ set -e
 
 mkdir -p output
 
-cd web/renderer
-yarn add preact --peer --frozen-lockfile
+# cd web/renderer
+# yarn add preact --peer --frozen-lockfile
 
-cd ../homepage
+cd web/homepage
 yarn --frozen-lockfile
+yarn link ../renderer
 yarn build
 
 cp -r build/* ../../output
